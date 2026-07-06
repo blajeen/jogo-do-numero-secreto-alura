e9de0c13f285b6be73222d5c2ae1ccb9463dc0dc
blajeen <brg.ftw@gmail.com>
2026-07-06 20:03:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+alert('bem vindo ao jogo do numero secreto');
 
+let numerosecreto = 5;
+let chute = prompt('digite um numero de 1 a 10');
+
+while (chute != 5){
+    if (chute < numerosecreto){
+        alert(`o numero secreto eh maior que ${chute}`);
+        }
+    if (chute > numerosecreto){
+      alert(`o numero secreto eh menor que ${chute}`);
+    }
+chute = prompt('tente novamente');
+    }
+          alert('voce acertou');
+        }
